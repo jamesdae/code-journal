8521118f3a08b921d9fa16ec41d2a9f05509fba3
James Yang <jamesdaeyang@gmail.com>
2022-11-01 22:04:27 +0000

    Added local storage functionality up to task 7; Issue #1

--- js/data.js
+++ js/data.js
@@ -7,19 +7,7 @@ var data = {
   nextEntryId: 1
 };
 
-var $entryForm = document.forms[0];
-var $titleInput = document.getElementById('titleinput');
-
-$entryForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  var newObject = {};
-  newObject.titleinput = $titleInput.value;
-  newObject.photoinput = document.forms[0].elements.photoinput.value;
-  newObject.notesinput = document.forms[0].elements.notesinput.value;
-  newObject.entryId = data.nextEntryId;
-  data.nextEntryId += 1;
-  data.entries.unshift(newObject);
-
-  // console.log(data);
-  $entryForm.reset();
+window.addEventListener('beforeunload', function () {
+  var formJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', formJSON);
 });
--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 var $submitPhoto = document.getElementById('photoinput');
-
 var $image = document.querySelector('img');
+var $entryForm = document.forms[0];
+var $titleInput = document.getElementById('titleinput');
 
 $submitPhoto.addEventListener('input', function (event) {
   $image.removeAttribute('src');
   $image.setAttribute('src', event.target.value);
-  // console.log(event.target.value);
+});
+
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var newObject = {};
+  newObject.titleinput = $titleInput.value;
+  newObject.photoinput = document.forms[0].elements.photoinput.value;
+  newObject.notesinput = document.forms[0].elements.notesinput.value;
+  newObject.entryId = data.nextEntryId;
+  data.nextEntryId += 1;
+  data.entries.unshift(newObject);
+  $image.removeAttribute('src');
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $entryForm.reset();
 });
