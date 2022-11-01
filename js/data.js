92ce538a363bf443032aace00ffde374a0f3dd75
James Yang <jamesdaeyang@gmail.com>
2022-11-01 21:37:22 +0000

    Updated event listeners up to task 6.4; Issue #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,20 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var $entryForm = document.forms[0];
+var $titleInput = document.getElementById('titleinput');
+
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var newObject = {};
+  newObject.titleinput = $titleInput.value;
+  newObject.photoinput = document.forms[0].elements.photoinput.value;
+  newObject.notesinput = document.forms[0].elements.notesinput.value;
+  newObject.entryId = data.nextEntryId;
+  data.nextEntryId += 1;
+  data.entries.unshift(newObject);
+
+  // console.log(data);
+  $entryForm.reset();
+});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var $submitPhoto = document.getElementById('photoinput');
+
 var $image = document.querySelector('img');
+
 $submitPhoto.addEventListener('input', function (event) {
   $image.removeAttribute('src');
   $image.setAttribute('src', event.target.value);
