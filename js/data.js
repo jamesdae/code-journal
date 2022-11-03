c0dea221936a24467b786fb555eabfa8ff7665d2
James Yang <jamesdaeyang@gmail.com>
2022-11-03 00:16:22 +0000

    Issue #2; Updated js files to prepend DOM tree before and after reload with same appearance; #4

--- js/data.js
+++ js/data.js
@@ -18,13 +18,46 @@ window.addEventListener('beforeunload', function (event) {
   this.localStorage.setItem('javascript-local-storage', dataJSON);
 });
 
-// function renderEntry(entry) {
-//   var $entryLi = document.createElement('li');
-//   $entryLi.setAttribute('class', 'column-half');
-//   var $imagediv = document.createElement('div');
-//   $imagediv.setAttribute('class', 'imagediv');
-//   $entryLi.appendChild($imagediv);
-//   var $entryimage = document.createElement('img');
-//   $entryimage.setAttribute('src', entry.photoinput);
-//   $imagediv.appendChild($entryimage);
-// }
+function renderEntry(entry) {
+  var $entryLi = document.createElement('li');
+  $entryLi.setAttribute('class', 'row');
+
+  var $firstcolumn = document.createElement('div');
+  $firstcolumn.setAttribute('class', 'column-half');
+  $entryLi.appendChild($firstcolumn);
+
+  var $imagediv = document.createElement('div');
+  $imagediv.setAttribute('class', 'imagediv');
+  $firstcolumn.appendChild($imagediv);
+
+  var $entryimage = document.createElement('img');
+  $entryimage.setAttribute('src', entry.photoinput);
+  $imagediv.appendChild($entryimage);
+
+  var $secondcolumn = document.createElement('div');
+  $secondcolumn.setAttribute('class', 'column-half');
+  $entryLi.appendChild($secondcolumn);
+
+  var $textDiv = document.createElement('div');
+  $textDiv.setAttribute('class', 'textdiv');
+  $secondcolumn.appendChild($textDiv);
+
+  var $entryheading = document.createElement('h2');
+  $entryheading.setAttribute('class', 'entryheading');
+  $entryheading.textContent = entry.titleinput;
+  $textDiv.appendChild($entryheading);
+
+  var $entrytext = document.createElement('p');
+  $entrytext.setAttribute('class', 'entrytext');
+  $entrytext.innerText = entry.notesinput;
+  $textDiv.appendChild($entrytext);
+  return $entryLi;
+}
+
+var $entryList = document.querySelector('ul.row');
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    $entryList.appendChild(renderEntry(data.entries[i]));
+  }
+});
--- js/main.js
+++ js/main.js
@@ -2,17 +2,20 @@ var $submitPhoto = document.getElementById('photoinput');
 var $image = document.getElementById('entryimg');
 var $entryForm = document.forms[0];
 var $titleInput = document.getElementById('titleinput');
-
-// lines 8 and 13-14 are temporary
-// img should be set from local storage in data.js
-
-var $firstimg = document.getElementById('firstimg');
+var $entryList = document.querySelector('ul.row');
 
 $submitPhoto.addEventListener('input', function (event) {
-  $image.removeAttribute('src');
-  $image.setAttribute('src', event.target.value);
-  $firstimg.removeAttribute('src');
-  $firstimg.setAttribute('src', event.target.value);
+  function isValidUrl(url) {
+    try {
+      return Boolean(new URL(event.target.value));
+    } catch (e) {
+      return false;
+    }
+  }
+  if (isValidUrl(event.target.value)) {
+    $image.removeAttribute('src');
+    $image.setAttribute('src', event.target.value);
+  }
 });
 
 $entryForm.addEventListener('submit', function (event) {
@@ -26,5 +29,42 @@ $entryForm.addEventListener('submit', function (event) {
   data.entries.unshift(newObject);
   $image.removeAttribute('src');
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $entryList.prepend(preEntry());
   $entryForm.reset();
 });
+
+function preEntry(entry) {
+  var $entryLi = document.createElement('li');
+  $entryLi.setAttribute('class', 'row');
+
+  var $firstcolumn = document.createElement('div');
+  $firstcolumn.setAttribute('class', 'column-half');
+  $entryLi.appendChild($firstcolumn);
+
+  var $imagediv = document.createElement('div');
+  $imagediv.setAttribute('class', 'imagediv');
+  $firstcolumn.appendChild($imagediv);
+
+  var $entryimage = document.createElement('img');
+  $entryimage.setAttribute('src', document.forms[0].elements.photoinput.value);
+  $imagediv.appendChild($entryimage);
+
+  var $secondcolumn = document.createElement('div');
+  $secondcolumn.setAttribute('class', 'column-half');
+  $entryLi.appendChild($secondcolumn);
+
+  var $textDiv = document.createElement('div');
+  $textDiv.setAttribute('class', 'textdiv');
+  $secondcolumn.appendChild($textDiv);
+
+  var $entryheading = document.createElement('h2');
+  $entryheading.setAttribute('class', 'entryheading');
+  $entryheading.textContent = $titleInput.value;
+  $textDiv.appendChild($entryheading);
+
+  var $entrytext = document.createElement('p');
+  $entrytext.setAttribute('class', 'entrytext');
+  $entrytext.innerText = document.forms[0].elements.notesinput.value;
+  $textDiv.appendChild($entrytext);
+  return $entryLi;
+}
